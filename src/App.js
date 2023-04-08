2a51447c4948185f3b7190aba31caf13661c5130
mishranisha-17 <nisha17soin@gmail.com>
2023-04-08 23:36:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  return (
+    //empty tag react fragments
+    <>
+       <div>
+        <p>Ladakh is a region administered by India as a union territory which constitutes a part of the larger Kashmir region and has been the subject of dispute between India, Pakistan, and China since 1947.
+        </p>
+         <p>Ladakh is most famous for breathtaking landscapes, the crystal clear skies, the highest mountain passes, thrilling adventure activities, Buddhist Monasteries and festivals.
+         </p>
+         <p>Set amidst the epic Himalayas, Ladakh is a rustic and heavenly beautiful travel destination. The rugged valleys and mountains, winding roads coupled with the vibrant cultural life maintain the exuberance and charm of this region.
+          </p>
+         
+</div> 
+    </>
+  )
+}
+     
+export default App;
+
+//reat liberary used for creating frontend
+//react is a virtual dom
+//react use jsx : javascript +xml(html)
+//babel : html, css and js
